e8a6de21c8a438a8ad21ef1389350a9427eeb735
Jael91 <jaelorea@gmail.com>
2021-01-14 11:34:36 -0600

    changes

--- src/main.js
+++ src/main.js
@@ -1,30 +1,38 @@
 //import { example } from './data.js';
 //import data from './data/pokemon/pokemon.js';
 
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './pokemon/pokemon.js';
+
 console.log(data.pokemon);
 
-const buttonStart = document.getElementById('btnStart');
-buttonStart.addEventListener('click', showNames);
+let card = document.getElementById('pokedexContainer1');
+let pokedex = document.getElementById('pokedexContainer2');
 
 let pokemonName = [];
 let pokemonNum = [];
 let pokemonImag = [];
 
+
 //imprimiendo en pantalla nombre del primer Pokemón
-/*function showNames() {
-  const allPokemon = data.pokemon;
-  let firstPokemon = allPokemon[2];
-  console.log(firstPokemon)
-  document.getElementById('pokedexContainer').innerHTML = firstPokemon.name
-}*/
+const allPokemon = data.pokemon;
+let firstPokemon = allPokemon('name');
+console.log(firstPokemon)
+card.innerHTML = firstPokemon.name;
+
+
+
+
+const buttonStart = document.getElementById('btnStart');
+buttonStart.addEventListener('click', showNames);
+
 
 //imprimiendo en pantalla los nombres de todos los Pokémon
 function showNames() {
   const allPokemon = data.pokemon;
-  for (let i = 0; i < allPokemon.length; i++) {
+    for (let i = 0; i < allPokemon.length; i++) {
     pokemonName[i] =  allPokemon[i].name;
   }
   console.log(pokemonName)
-  document.getElementById('pokedexContainer').innerHTML = pokemonName;
+  pokedex.innerHTML = pokemonName;
 }
